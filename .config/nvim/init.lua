acbea073192028cc662101e4bca24208defe497a
Lucas M. Campos <lucasm.campos@hotmail.com.br>
2024-12-07 14:22:37 -0300

    fix init.lua and add help.txt

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,7 +4,7 @@ local load_plugins = true
 
 if load_plugins then
     vim.cmd [[packadd packer.nvim]]
+    require('plugins')
     require('keymaps')
     require('theme')
-    require('plugins')
 end
